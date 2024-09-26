23e8fab1fcec71072983b0555dbb0586e795295c
rajonhossaindhaka <rajonhossaindhaka@gmail.com>
2024-09-26 20:24:27 +0600

    tesss

---
+++ resources/views/components/leftmenu_tree.php
@@ -0,0 +1,40 @@
+
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .sidebar {
+            width: 250px;
+            height: 100vh;
+            background-color: #333;
+            color: white;
+            position: fixed;
+            left: 0;
+            top: 0;
+            padding: 20px;
+        }
+        .sidebar ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+        .sidebar ul li {
+            padding: 10px;
+            cursor: pointer;
+        }
+        .sidebar ul li:hover {
+            background-color: #444;
+        }
+        .submenu {
+            display: none;
+            padding-left: 20px;
+        }
+        .submenu li {
+            padding: 8px 10px;
+            background-color: #555;
+        }
+        .submenu li:hover {
+            background-color: #666;
+        }
+    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,8 +49,12 @@
                             class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">
                             Submit
                         </button>
+
+
                     </div>
                 </form>
+
+              
 
             </div>
         </main>
